7b1a18b6d6d7b816fbb45027cdfd77629c27454d
larry <rex@RexdeMacBook-Pro.local>
2014-02-27 17:59:16 +0800

    Initial Commit

---
+++ PeopleDatabase/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  PeopleDatabase
+//
+//  Created by larry on 14-2-27.
+//  Copyright (c) 2014年 larry. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
